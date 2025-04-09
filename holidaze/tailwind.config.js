b053d11d8bd9a891645372ab8ff2dc07b60a7865
natnoppol <126169607+natnoppol@users.noreply.github.com>
2025-04-10 00:39:56 +0200

    update tailwind

---
+++ holidaze/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js (updated for ES module)
+export default {
+    content: [
+      './src/**/*.{html,js,jsx,ts,tsx}',
+      './public/index.html',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
