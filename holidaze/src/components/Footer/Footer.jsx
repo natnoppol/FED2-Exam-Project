557670a2e3b6b5cf2db5511712963d98ab906d94
natnoppol <126169607+natnoppol@users.noreply.github.com>
2025-05-10 17:34:41 +0200

    Update Footer.jsx

--- holidaze/src/components/Footer/Footer.jsx
+++ holidaze/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import FooterBottom from "./FooterBottom";
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-5 px-5 mt-10">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 place-items-center">
+       <div className="max-w-7xl mx-auto grid grid-cols-1 sm:place-items-center md:place-items-center lg:place-items-center xl:place-items-center">
         <FooterBrand />
       </div>
       <FooterBottom />
