4e72ea36eb19fb2cb54e1779ca0dbb5e059cbd45
natnoppol <126169607+natnoppol@users.noreply.github.com>
2025-05-10 17:26:25 +0200

    updated adminbooking and little footer

--- holidaze/src/components/Footer/Footer.jsx
+++ holidaze/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import FooterBottom from "./FooterBottom";
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-10 px-4 mt-10">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 gap-8">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 place-items-center">
         <FooterBrand />
       </div>
       <FooterBottom />
