da22ceaa5ae4f19fef5267bb1aa9d27a501b4860
natnoppol <126169607+natnoppol@users.noreply.github.com>
2025-05-10 17:34:15 +0200

    update footer

--- holidaze/src/components/Footer/Footer.jsx
+++ holidaze/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import FooterBottom from "./FooterBottom";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-10 px-4 mt-10">
+    <footer className="bg-gray-900 text-white py-5 px-5 mt-10">
       <div className="max-w-7xl mx-auto grid grid-cols-1 place-items-center">
         <FooterBrand />
       </div>
