bdcef957d8799bd2987dbdca8a5c1a83dcc6a3be
natnoppol <126169607+natnoppol@users.noreply.github.com>
2025-05-10 17:30:39 +0200

    Update FooterBrand.jsx

--- holidaze/src/components/Footer/FooterBrand.jsx
+++ holidaze/src/components/Footer/FooterBrand.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const FooterBrand = () => (
-    <div>
-      <Link
-            to="/"
-            className="text-2xl font-extrabold tracking-tight hover:text-indigo-200 transition-colors duration-200"
-          >
-            Holidaze
-          </Link>
-      <p className="text-gray-400">
-        Discover unique stays and unforgettable experiences with Holidaze.
-      </p>
-    </div>
-  );
-  
-  export default FooterBrand;
-  
+  <div className="text-center space-y-2">
+    <Link
+      to="/"
+      className="text-2xl font-extrabold tracking-tight hover:text-indigo-200 transition-colors duration-200"
+    >
+      Holidaze
+    </Link>
+    <p className="text-gray-400">
+      Discover unique stays and unforgettable experiences with Holidaze.
+    </p>
+  </div>
+);
+
+export default FooterBrand;
