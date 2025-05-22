9dced60bdad3d1bb619f0e504b00dd361933100f
natnoppol <126169607+natnoppol@users.noreply.github.com>
2025-05-22 02:56:33 +0200

    Update main.jsx

--- holidaze/src/main.jsx
+++ holidaze/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { UserProvider } from "./contexts/UserContext";
-import { VenueProvider } from "./contexts/venueContext"; 
+// check 
+import { VenueProvider } from "./contexts/VenueContext"; // Import VenueProvider
+
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
