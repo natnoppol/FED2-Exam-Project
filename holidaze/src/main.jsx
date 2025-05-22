59e6ddcdfaf4d963ab4fef08732c1b605b276469
natnoppol <126169607+natnoppol@users.noreply.github.com>
2025-05-22 02:46:49 +0200

    Update main.jsx

--- holidaze/src/main.jsx
+++ holidaze/src/main.jsx
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { UserProvider } from "./contexts/UserContext";
-
-// new line
-import { VenueProvider } from "./contexts/venueContext"; // Import VenueProvider
+import { VenueProvider } from "./contexts/venueContext"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
